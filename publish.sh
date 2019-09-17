520f6b58a3964faec09d099bdc30e487de583d48
Yuriy Kuzin <yuriy.kuzin@gmail.com>
2019-09-17 11:37:39 +0300

    upgrade publish.sh

--- publish.sh
+++ publish.sh
@@ -13,7 +13,11 @@ git checkout -b releases/v1   # If this branch already exists, omit the -b flag
 npm i
 npm run build
 rm -rf node_modules
-sed -i '' '/node_modules/d' .gitignore   # Bash command that removes node_modules from .gitignore
+
+# Bash command that removes node_modules from .gitignore
+# modified to work both on Linux and MacOS
+sed -i.bak '/node_modules/d' .gitignore && rm .gitignore.bak
+
 npm install --production
 git add node_modules .gitignore
 git commit -m node_modules
